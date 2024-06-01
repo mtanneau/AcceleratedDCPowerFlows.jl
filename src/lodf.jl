7db1d1b195244ba15c0b9224a069112c348e19e5
mtanneau <mathieu.tanneau@gmail.com>
2024-06-01 10:20:02 -0400

    Add LazyLODF + missing code for Full PTDF

--- src/lodf.jl
+++ src/lodf.jl
@@ -28,3 +28,53 @@ function FullLODF(network)
     return FullLODF(Φ.N, Φ.E, M)
 end
 
+struct LazyLODF{TF} <: AbstractLODF
+    N::Int
+    E::Int
+    
+    Φ::LazyPTDF{TF}
+end
+
+function LazyLODF(data; solver::Symbol=:klu)
+    Φ = LazyPTDF(data; solver=solver)
+    return LazyLODF(Φ.N, Φ.E, Φ)
+end
+
+function LazyLODF(Φ::LazyPTDF)
+    return LazyLODF(Φ.N, Φ.E, Φ)
+end
+
+function compute_flow!(pf, p::Vector, L::LazyLODF, k::Int)
+    Φ = L.Φ
+    i0 = Φ.islack
+    # TODO: give option to pass θ0 or pf0 as argument
+    # This would save one linear system solve
+    θ0 = -(Φ.F \ p)  # Lazy PTDF factorizes -(AᵀBA), so we need to negate
+    θ0[i0] = 0       # slack bus has zero phase angle
+
+    # Next, we need to compute (AᵀBA)⁻¹ aₖ, where aₖ is the kth row of A
+    a = Vector(Φ.A[k, :])  # ⚠ most linear solvers wrappers do not support passing a sparse RHS here
+    bk = Φ.b[k]
+    ξ = -(Φ.F \ a)  # Lazy PTDF factorizes -(AᵀBA), so we need to negate
+    ξ[i0] = 0       # slack bus has zero phase angle
+
+    # Compute post-update phase angles via SWM formula
+    θc = θ0 + (dot(a, θ0) * bk / (1 - bk*dot(a, ξ))) .* ξ
+    θc[i0] = 0
+
+    # Re-multiply by BA, and zero-out the kth element
+    mul!(pf, Φ.BA, θc)
+    pf[k] = 0
+
+    return pf
+end
+
+function compute_all_flows!(pf, p, pf0, L::FullLODF; outages=Int[])
+    N, E = L.N, L.E
+
+    for (i, l) in enumerate(outages)
+        pf[:, i] .= pf0 .+ (pf0[l] .* L.matrix[:, l])
+    end
+
+    return pf
+end
--- test/lodf.jl
+++ test/lodf.jl
@@ -38,6 +38,44 @@ function test_full_lodf()
     return nothing
 end
 
+function test_lazy_lodf()
+    data = PM.make_basic_network(pglib("pglib_opf_case5_pjm"))
+    N = length(data["bus"])
+    E = length(data["branch"])
+    p = real.(calc_basic_bus_injection(data))
+
+    is_bridge = FP.find_bridges(data)
+    outages = collect(1:E)[.! is_bridge]
+    K = length(outages)
+
+    Φ = calc_basic_ptdf_matrix(data)
+
+    # Compute LODF
+    L = FP.LazyLODF(data)
+    pf_fp = zeros(E)
+
+    for (i, k) in enumerate(outages)
+        br = data["branch"]["$k"]
+        _r = br["br_r"]
+
+        # Set branch resistance to Inf --> will zero-out the flow
+        br["br_r"] = Inf
+        # Re-compute power flows
+        Φk = calc_basic_ptdf_matrix(data)
+        pf_pm = Φk * p
+        
+        FP.compute_flow!(pf_fp, p, L, k)
+
+        @test isapprox(pf_pm, pf_fp, atol=1e-6)
+
+        # reset branch resistance
+        br["br_r"] = _r
+    end
+
+    return nothing
+end
+
 @testset "LODF" begin
     @testset test_full_lodf()
+    @testset test_lazy_lodf()
 end
