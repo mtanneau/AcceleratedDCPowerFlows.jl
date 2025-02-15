ef40d13ecce35ce2324491a40abb810326365018
mtanneau <mathieu.tanneau@gmail.com>
2025-02-15 18:47:24 -0500

    Fix bridge computation to handle parallel branches

--- src/graphs.jl
+++ src/graphs.jl
@@ -3,20 +3,32 @@ using Graphs
 function find_bridges(data)
     N = length(data["bus"])
     E = length(data["branch"])
-    # Identify bridges
-    G = Graph(N)
-    edge2id = Dict(
-        (data["branch"]["$k"]["f_bus"], data["branch"]["$k"]["t_bus"]) => k
-        for k in 1:E
-    )
-    for ((i, j), k) in edge2id
-        add_edge!(G, i, j)
+
+    # Identify all parallel branches
+    edge2branches = Dict{Tuple{Int,Int},Set{Int}}()
+    branch2edge   = Dict{Int,Tuple{Int,Int}}()
+    for k in 1:E
+        br = data["branch"]["$k"]
+        i, j = extrema((br["f_bus"], br["t_bus"]))
+
+        s = get!(edge2branches, (i, j), Set{Int}())
+        push!(s, k)
+        branch2edge[k] = (i, j)
     end
 
-    is_bridge = falses(E)
+    # Identify bridges
+    G = Graph(N)
+    for ((i, j), k) in edge2branches
+        add_edge!(G, i, j)
+    end
+    
+    is_bridge = zeros(Bool, E)
     for edge in bridges(G)
-        k = edge2id[edge.src, edge.dst]
-        is_bridge[k] = true
+        i, j = (edge.src, edge.dst)
+        s = edge2branches[(i, j)]
+        for k in s
+            is_bridge[k] = (length(s) == 1)
+        end
     end
 
     return is_bridge
