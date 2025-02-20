b7e921f4862903dc0a810c548abe990a95873b3b
mtanneau <mathieu.tanneau@gmail.com>
2025-02-20 15:14:50 -0500

    Additional batch PTDF tests

--- test/ptdf.jl
+++ test/ptdf.jl
@@ -14,6 +14,14 @@ function test_ptdf_full()
     FP.compute_flow!(f, p, Φ)  
     @test isapprox(f, fpm; atol=1e-6)
 
+    # Check again with batched mode
+    K = 4
+    p = randn(N, K)
+    f = zeros(E, K)
+    fpm = Φ_pm * p
+    FP.compute_flow!(f, p, Φ)
+    @test isapprox(f, fpm; atol=1e-6)
+
     return nothing
 end
 
@@ -28,6 +36,11 @@ function test_ptdf_lazy()
     # Reference power flows, computed with PowerModels
     Φ_pm = PM.calc_basic_ptdf_matrix(data)
     fpm = Φ_pm * p
+
+    K = 4
+    p_batch = randn(N, K)
+    f_batch = zeros(E, K)
+    f_batch_pm = Φ_pm * p_batch
     
     @testset "$solver" for solver in [:cholesky, :ldlt, :lu, :klu]
         Φ = FP.LazyPTDF(data; solver, gpu=false)
@@ -35,6 +48,10 @@ function test_ptdf_lazy()
         # Check our power flows against PowerModels
         FP.compute_flow!(f, p, Φ)
         @test isapprox(f, fpm; atol=1e-6)
+
+        # Check batched mode
+        FP.compute_flow!(f_batch, p_batch, Φ)
+        @test isapprox(f_batch, f_batch_pm; atol=1e-6)
     end
 
     return nothing
