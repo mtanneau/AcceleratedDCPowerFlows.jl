1815397984b40bfe83f0f212b3939be38a1d1f1d
mtanneau <mathieu.tanneau@gmail.com>
2026-02-18 13:34:29 -0500

    Re-organize test code structure

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,15 @@ using FastPowerFlow
 const FP = FastPowerFlow
 
 @testset "FastPowerFlow" begin
-    include("branch_incidence_matrix.jl")
-    include("ptdf.jl")
-    include("lodf.jl")
+    @testset "core" begin
+        include("core/branch_incidence_matrix.jl")
+    end
+
+    @testset "PTDF" begin
+        include("ptdf/ptdf.jl")
+    end
+
+    @testset "LODF" begin
+        include("lodf/lodf.jl")
+    end
 end
