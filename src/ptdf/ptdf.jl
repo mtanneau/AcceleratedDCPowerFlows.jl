2ec996642af871ce99cbd46c223bb7321e668049
mtanneau <mathieu.tanneau@gmail.com>
2026-02-25 12:33:42 -0500

    Remove dead code

--- src/ptdf/ptdf.jl
+++ src/ptdf/ptdf.jl
@@ -1,19 +1,5 @@
 abstract type AbstractPTDF end
 
-function _linear_solver(s)
-    if s == :lu
-        return lu
-    elseif s == :klu
-        return KLU.klu
-    elseif s == :ldlt
-        return ldlt
-    elseif s == :cholesky
-        return cholesky
-    else
-        error("Invalid linear solver: only lu, klu (CPU only), ldlt, cholesky are supported")
-    end
-end
-
 function ptdf(network::Network;
     backend=DefaultBackend(),
     linear_solver=:auto,
