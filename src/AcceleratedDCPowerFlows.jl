195ebb228f538d75e69a0b8ee167265ceaf13279
mtanneau <mathieu.tanneau@gmail.com>
2026-02-24 23:12:23 -0500

    Add KernelAbstractions to deps

--- src/AcceleratedDCPowerFlows.jl
+++ src/AcceleratedDCPowerFlows.jl
@@ -4,6 +4,8 @@ using LinearAlgebra
 using SparseArrays
 using SuiteSparse
 
+import KernelAbstractions as KA
+
 using KLU
 
 export Network
@@ -19,25 +21,5 @@ include("core/branch_incidence_matrix.jl")
 include("graphs.jl")
 include("ptdf/ptdf.jl")
 include("lodf/lodf.jl")
-
-function BranchIncidenceMatrix(data::Dict)
-    N::Int = length(data["bus"])
-    E::Int = length(data["branch"])
-
-    bus_fr = Vector{Int}(undef, E)
-    bus_to = Vector{Int}(undef, E)
-    for e in 1:E
-        bus_fr[e] = data["branch"]["$e"]["f_bus"]
-        bus_to[e] = data["branch"]["$e"]["t_bus"]
-    end
-    
-    # data checks
-    imin, imax = extrema(bus_fr)
-    1 <= imin <= imax <= N || throw(ArgumentError("bus_fr out of bounds"))
-    jmin, jmax = extrema(bus_to)
-    1 <= jmin <= jmax <= N || throw(ArgumentError("bus_to out of bounds"))
-
-    return BranchIncidenceMatrix(N, E, bus_fr, bus_to)
-end
 
 end  # module
