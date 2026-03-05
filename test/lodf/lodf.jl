33effc557722f58f35ef504c4a7a7ff5253b8999
mtanneau <mathieu.tanneau@gmail.com>
2026-03-05 10:38:09 -0500

    Add tests for PTDF/LODF entry points

--- test/lodf/lodf.jl
+++ test/lodf/lodf.jl
@@ -1,7 +1,22 @@
 include("full.jl")
 include("lazy.jl")
 
+
+function test_lodf_entry_points()
+    data = PM.make_basic_network(pglib("pglib_opf_case14_ieee"))
+    network = APF.from_power_models(data)
+
+    M = APF.lodf(network; lodf_type=:full)
+    @test isa(M, APF.FullLODF)
+
+    M = APF.lodf(network; lodf_type=:lazy)
+    @test isa(M, APF.LazyLODF)
+
+    @test_throws ErrorException APF.lodf(network; lodf_type=:other)
+end
+
 @testset "LODF" begin
     @testset test_full_lodf()
     @testset test_lazy_lodf()
+    @testset test_lodf_entry_points()
 end
--- test/ptdf/ptdf.jl
+++ test/ptdf/ptdf.jl
@@ -1,7 +1,24 @@
 include("full.jl")
 include("lazy.jl")
 
+function test_ptdf_entry_points()
+    data = PM.make_basic_network(pglib("pglib_opf_case14_ieee"))
+    network = APF.from_power_models(data)
+
+    M = APF.ptdf(network; ptdf_type=:full)
+    @test isa(M, APF.FullPTDF)
+
+    M = APF.ptdf(network; ptdf_type=:lazy)
+    @test isa(M, APF.LazyPTDF)
+
+    @test_throws ErrorException APF.ptdf(network; ptdf_type=:other)
+
+    @test_throws MethodError APF.full_ptdf(network; ptdf_type=:lazy)
+    @test_throws MethodError APF.lazy_ptdf(network; ptdf_type=:full) 
+end
+
 @testset "PTDF" begin
     @testset test_ptdf_full()
     @testset test_ptdf_lazy()
+    @testset test_ptdf_entry_points()
 end
