79fd66dba34a6e6ba8fee56f749b182517e4405a
mtanneau <mathieu.tanneau@gmail.com>
2026-03-09 11:27:06 -0400

    Trigger bridge issue in tests
    Signed-off-by: mtanneau <mathieu.tanneau@gmail.com>

--- test/graph/bridges.jl
+++ test/graph/bridges.jl
@@ -18,4 +18,19 @@ function test_bridges()
     return nothing
 end
 
+function test_bridges_reverse_orientation()
+    data = PM.make_basic_network(pglib("pglib_opf_case14_ieee"))
+    network = APF.from_power_models(data)
+    b = APF.find_bridges(network)
+
+    # Now reverse all branches
+    for br in network.branches
+        br.bus_fr, br.bus_to = br.bus_to, br.bus_fr
+    end
+    b_rev = APF.find_bridges(network)
+
+    @test b == b_rev
+end
+
 test_bridges()
+test_bridges_reverse_orientation()
